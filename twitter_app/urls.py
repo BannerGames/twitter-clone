7bc96c26406fee30197bbfeb702276a8f2b17736
BannerGames <davidandrade339@gmail.com>
2019-08-18 20:56:18 +0100

    Update urls.py

--- twitter_app/urls.py
+++ twitter_app/urls.py
@@ -7,26 +7,26 @@ urlpatterns = [
     path('', views.index, name='index'),
 
     # Thanks Page
-    path('thanks/', views.thanks, name='thanks'),
+    path('thanks/', views.thanks, name='sucesso'),
 
     # Profile Page
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.profile, name='perfil'),
 
     # Following and Followers Page
-    path('follow_info/', views.follow_info, name='follow_info'),
+    path('follow_info/', views.follow_info, name='seguindo'),
 
     # Settings Page
-    path('profile_settings/', views.profile_settings, name='profile_settings'),
+    path('profile_settings/', views.profile_settings, name='configuracoes'),
 
     # Other User Page
-    path('other_user/<str:username>/', views.other_user_profile, name='other_user_profile'),
+    path('other_user/<str:username>/', views.other_user_profile, name='usuario'),
 
     # Other User Follow info page
-    path('other_user/<str:username>/follow_info/', views.other_user_follow_info, name='other_user_follow_info'),
+    path('other_user/<str:username>/follow_info/', views.other_user_follow_info, name='usuario_seguindo'),
 
     # Home page for recent tweets
-    path('home/', views.home, name='home'),
+    path('home/', views.home, name='inicio'),
 
     # Explore page
-    path('explore/', views.explore, name='explore')
+    path('explore/', views.explore, name='explorar')
 ]
