ae6e52c134185481e094494582ebb62a15d73695
BannerGames <davidandrade339@gmail.com>
2019-08-19 11:42:14 +0100

    Update settings.py

--- entry_folder/settings.py
+++ entry_folder/settings.py
@@ -1,4 +1,3 @@
-import Django
 import os
 import cloudinary
 
@@ -143,7 +142,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # The URL to use when referring to static files (where they will be served from)
 STATIC_URL = '/static/'
-Django.settings(locals())
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
